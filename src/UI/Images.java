433ef7fccd666b97fc0ba70468e8d8865c10cd8b
Thanawat <taan02991@hotmail.com>
2018-11-09 17:07:09 +0700

    edit directory of images in bin folder

--- src/UI/Images.java
+++ src/UI/Images.java
@@ -5,9 +5,9 @@ import javafx.scene.image.ImageView;
 
 public class Images {
 
-		public static final Image startBackground = new Image(ClassLoader.getSystemResource("StartScene.png").toString());
-		public static final Image startButton = new Image(ClassLoader.getSystemResource("StartButton.png").toString());
-		public static final Image startButtonHover = new Image(ClassLoader.getSystemResource("StartButtonHover.png").toString());
+		public static final Image startBackground = new Image(ClassLoader.getSystemResource("images/StartScene.png").toString());
+		public static final Image startButton = new Image(ClassLoader.getSystemResource("images/StartButton.png").toString());
+		public static final Image startButtonHover = new Image(ClassLoader.getSystemResource("images/StartButtonHover.png").toString());
 		
 
 }
