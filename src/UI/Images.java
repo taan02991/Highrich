37adf8c7ccd7c209f022d2b8112d83a9a1d8ce93
Thanawat <taan02991@hotmail.com>
2018-11-12 12:38:07 +0700

    Rename images folder

--- src/UI/Images.java
+++ src/UI/Images.java
@@ -12,10 +12,10 @@ public class Images {
 		public static final Image startButtonHover = new Image(ClassLoader.getSystemResource("images/StartButtonHover.png").toString());
 		public static final Image floor = new Image(ClassLoader.getSystemResource("images/floor.png").toString());
 		//Add Player PIC later
-		public static final Image[] playerL = {new Image(ClassLoader.getSystemResource("Images/PlayerL.png").toString())};
-		public static final Image[] playerR = {new Image(ClassLoader.getSystemResource("Images/PlayerR.png").toString())};
-		public static final Image[] playerU = {new Image(ClassLoader.getSystemResource("Images/PlayerU.png").toString())};
-		public static final Image[] playerD = {new Image(ClassLoader.getSystemResource("Images/PlayerD.png").toString())};
+		public static final Image[] playerL = {new Image(ClassLoader.getSystemResource("images/PlayerL.png").toString())};
+		public static final Image[] playerR = {new Image(ClassLoader.getSystemResource("images/PlayerR.png").toString())};
+		public static final Image[] playerU = {new Image(ClassLoader.getSystemResource("images/PlayerU.png").toString())};
+		public static final Image[] playerD = {new Image(ClassLoader.getSystemResource("images/PlayerD.png").toString())};
 		//
 		public static final Image tree = new Image(ClassLoader.getSystemResource("images/Tree.png").toString());
 		public static final Image sofa = new Image(ClassLoader.getSystemResource("images/Sofa.png").toString());
